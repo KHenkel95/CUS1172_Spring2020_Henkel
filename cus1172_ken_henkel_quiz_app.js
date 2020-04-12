9f1a664146a0987e47a72b8ed9cdb2f410548de3
Ken Henkel <kenhenkel95@gmail.com>
2020-04-12 16:31:04 -0400

    Changed quiz.html to index.html

--- cus1172_ken_henkel_quiz_app.js
+++ cus1172_ken_henkel_quiz_app.js
@@ -1,13 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector("input").onchange = q;
+})
 
-});
-
-const render_view = (view) => {
-    template_source = document.querySelector(view).innerHTML
-    console.log(template_source)
-
-    var template = Handlebars.compile(template_souirce);
-
-    console.log({...model,...appState})
-    var html_widget_element = template({...model,...appState})
+function q(){
+    document.querySelector(#name).innerHTML = #quiz_selection;
 }
