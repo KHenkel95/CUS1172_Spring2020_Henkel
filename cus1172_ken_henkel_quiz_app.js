8ce0615bb92d1507467c129e7c1afb9056e85fc6
Ken Henkel <kenhenkel95@gmail.com>
2020-04-12 21:58:19 -0400

    Added loadJson function

--- cus1172_ken_henkel_quiz_app.js
+++ cus1172_ken_henkel_quiz_app.js
@@ -5,3 +5,9 @@ document.addEventListener('DOMContentLoaded', () => {
 function q(){
     document.querySelector(#name).innerHTML = #quiz_selection;
 }
+
+function loadJson(){
+    fetch('https://jsonplaceholder.typicode.com/KHenkel95/quizdata')
+        .then(response => response.json())
+        .then(json => console.log(json))
+}
