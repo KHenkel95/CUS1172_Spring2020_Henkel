0f1a185f8852c4c9646ed51dba0084064b77b701
Ken Henkel <kenhenkel95@gmail.com>
2020-05-04 21:48:22 -0400

    Update Frontend to Recycle after Quiz correctly

--- cus1172_ken_henkel_quiz_app2.js
+++ cus1172_ken_henkel_quiz_app2.js
@@ -2,13 +2,18 @@ window.onload = loadJson
 
 const apiUrl = "https://cus1172-henkel-quiz-app.herokuapp.com"
 
+var quizListJson = {};
+var currentQuestionJson = {};
+var quizId = "-1";
+var nextQuestionId = "first";
+var currentQuestion = {}
+
 async function loadJson(){
-    json = null;
     document.getElementById("quiz_menu").style.visibility = "hidden";
     document.getElementById("quiz_view").style.visibility = "hidden";
     response = await fetch(apiUrl + "/api/quiz/list")
     if (response.ok){
-        json=await response.json()
+        quizListJson=await response.json()
     }
     counter = 0;
 }
@@ -23,12 +28,16 @@ function clickEnter(){
     element.parentNode.removeChild(element);
     //makes the quiz menu visible
     document.getElementById("quiz_menu").style.visibility = "visible";
-    console.log(json)
 }
 
 function startQuiz(quizChoice){
     score = 0;
-    quizchoice = quizChoice;
+    if(quizChoice === "java"){
+        quizId = 100
+    }
+    else{
+        quizId = 200
+    }
     document.getElementById("mc_buttons").style.visibility = "hidden";
     document.getElementById("fib_input").style.visibility = "hidden";
     quizMenuElement = document.querySelector("#quiz_menu");
@@ -43,9 +52,8 @@ function startQuiz(quizChoice){
 
 function checkQuestionType(){
     questionType = null
-    console.log(json);
-    //TODO change this to check data.question_type
-    if(currentQuestion + ".data.question_type"==="mc") {
+
+    if(currentQuestion.question_type ==="mc") {
         questionType = "mc";
         document.getElementById("mc_buttons").style.visibility = "visible";
     }
@@ -61,58 +69,56 @@ function fibEnter(){
     return checkAnswer(document.querySelector("#fib_space").value);
 }
 
-function checkAnswer(answer){
-    console.log("answer inputed: " + answer);
-    //TODO change this to answers route answers.correct_answer
-    console.log("expected answer : " + currentQuestion.answer);
-    if(answer !== currentQuestion.answer){
-        //TODO change this to answers route answers.feedback
-        document.querySelector("#result").innerHTML = "INCORRECT: Sorry, you answered this incorrectly. HINT:" + currentQuestion.hint;
+async function checkAnswer(answer){
+    response = await fetch(apiUrl + `/api/check_answer/${quizId}/${currentQuestion.id}/${answer}`)
+    if (response.ok) {
+        answerResults = await response.json();
+        if(!(answerResults.correct)){
+            document.querySelector("#result").innerHTML = "INCORRECT: Sorry, you answered this incorrectly. HINT:" + answerResults.feedback;
 
-    }
-    else{
-        document.getElementById("mc_buttons").style.visibility = "hidden";
-        document.getElementById("fib_input").style.visibility = "hidden";
-        document.querySelector("#result").innerHTML = "Correct!";
-        setTimeout(function(){
-            document.querySelector("#result").innerHTML = "";
-            score++;
-            updateScoreboard();
-            getNextQuestion();
-        },1000 );
+        }
+        else{
+            document.getElementById("mc_buttons").style.visibility = "hidden";
+            document.getElementById("fib_input").style.visibility = "hidden";
+            document.querySelector("#result").innerHTML = "Correct!";
+            setTimeout(function(){
+                document.querySelector("#result").innerHTML = "";
+                score++;
+                updateScoreboard();
+                getNextQuestion();
+            },1000 );
 
+        }}else{
+        alert("unexpected api error")
     }
 }
 
-function getNextQuestion(){
-
-    if(quizchoice==="java"){
-        //TODO have these access next question
-        //currentQuestion = json.java.questions[counter]
-        currentQuestion = json + "/api/quiz/100/10" + [counter]
-        nextQuestion = currentQuestion + ".meta.next_question"
-    }
-    else{
-        //currentQuestion = json.golang.questions[counter]
-        currentQuestion = json + "/api/quiz/200/2000"
-    }
-    //TODO have this check if next_question is -1 to end quiz
-    //if(currentQuestion.meta.next_question===-1){
-    if(nextQuestion===-1){
+async function getNextQuestion(){
+    if(nextQuestionId === "-1"){
         endQuiz();
     }
     else{
-        //TODO get this to access the actual question data
-        document.getElementById("question").innerHTML = currentQuestion + ".data.question";
-        checkQuestionType();
-        counter++;
+        response = await fetch(apiUrl + `/api/quiz/${quizId}/${nextQuestionId}`)
+        if (response.ok) {
+            currentQuestionJson = await response.json();
+            if(currentQuestionJson.length > 0)
+                console.log(currentQuestionJson)
+            currentQuestion = currentQuestionJson.data
+            nextQuestionId = currentQuestionJson.meta.next_question;
+            document.getElementById("question").innerHTML = currentQuestion.question;
+            checkQuestionType();
+            counter++;
+        }
+        else{
+            alert("unexpected api error");
+        }
     }
 }
 
 function endQuiz(){
 
-    alert("End quiz");
-    counter = 0;
+    alert("End quiz, reloading page, good job!");
+    location.reload();
 
 }
 
